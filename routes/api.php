1673678d3a2c363c1aaf6b8e088ef8776b24f9b4
Jackylyn1 <49787310+Jackylyn1@users.noreply.github.com>
2024-10-20 18:54:16 +0200

    added request to api

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ApiRegisterController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::controller(RegisterController::class)->group(function(){
+Route::controller(ApiRegisterController::class)->group(function(){
     Route::post('login','login');
 });
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check()) return view('welcome');
+    return view('login');
+});
+
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('login','login')->name('login');
 });
