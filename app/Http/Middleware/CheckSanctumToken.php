49d95cc9cf202e86d8c2c3c5de9da7083827e982
Jackylyn1 <49787310+Jackylyn1@users.noreply.github.com>
2024-10-21 16:32:54 +0200

    added Logout in Backend and middleware

---
+++ app/Http/Middleware/CheckSanctumToken.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\RedirectResponse;
+class CheckSanctumToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response | \Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next):  Response | RedirectResponse
+    {
+        if (!$request->session()->has('sanctum_token'))
+            return response()->view('login');
+        return $next($request);
+    }
+}
