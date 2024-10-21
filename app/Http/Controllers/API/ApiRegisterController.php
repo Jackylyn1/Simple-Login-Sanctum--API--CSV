7fc4b28b32e0c150bc017e0bfd73771c06aa9016
Jackylyn1 <49787310+Jackylyn1@users.noreply.github.com>
2024-10-21 16:56:42 +0200

    removed Basecontroller because its only used once in API

--- app/Http/Controllers/API/ApiRegisterController.php
+++ app/Http/Controllers/API/ApiRegisterController.php
@@ -1,15 +1,49 @@
 <?php
 
 namespace App\Http\Controllers\API;
-use App\Http\Controllers\API\BaseController as BaseController;
 use Illuminate\Support\Facades\Auth;
 use Validator;
+use App\Http\Controllers\Controller;
 use Laravel\Sanctum\PersonalAccessToken;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class ApiRegisterController extends BaseController
+class ApiRegisterController extends Controller
 {
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendResponse(string $message, array $result): JsonResponse
+    {
+        return $this->sendStandardResponse($message, $result, 200);
+    }
+  
+    /**
+     * return error response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendError (string $error, array $errorMessages = [], $code = 404):JsonResponse
+    {       
+        return $this->sendStandardResponse($error, $errorMessages, $code);
+    }
+
+    /**
+     * return standard response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    private function sendStandardResponse(string $message, array $result, int $code):JsonResponse
+    {
+        $response = [
+            'success' => (($code == 200)?true:false),
+            'message' => $message,
+        ];
+        $response['data'] = $result??[];  
+        return response()->json($response, $code);
+    }
 
     /**
      * Logout in Backend
