ba3f0c87cf08bebbd0c73fb2fb23fc1556e8280b
Jackylyn1 <49787310+Jackylyn1@users.noreply.github.com>
2024-10-27 18:56:59 +0100

    added purge-css

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,2 +1,16 @@
 const mix = require('laravel-mix');
-mix.sass('resources/sass/app.scss', 'public/css');
+const purgecss = require('@fullhuman/postcss-purgecss');
+mix.js('resources/js/app.js', 'public/js')
+.sass('resources/sass/app.scss', 'public/css')
+.options({
+    postCss: [
+       purgecss({
+          content: [
+             './resources/views/**/*.blade.php',
+             './resources/js/**/*.vue',
+             './resources/js/**/*.js',
+          ],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+       })
+    ]
+});
